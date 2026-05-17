2314c7ad28dc22885f448761dd43686a1274c4ac
afreen nisha <nisha.hussain0310@gmail.com>
2026-05-17 16:21:33 +0530

    uploaded java internship task

---
+++ Task 4/ExceptionCalculator.java
@@ -0,0 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Enter first number: ");
+
+            int num1 = sc.nextInt();
+
+            System.out.print("Enter second number: ");
+
+            int num2 = sc.nextInt();
+
+            int result = num1 / num2;
+
+            System.out.println("Result = " + result);
+        }
+
+        catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero");
+        }
+
+        catch (InputMismatchException e) {
+
+            System.out.println("Invalid Input");
+        }
+
+        finally {
+
+            System.out.println("Program Finished");
+
+            sc.close();
+        }
+    }
+}
